8d1072914ffd98c87940c463688f83275aada126
黄 敏强 <nhhmq@gwinsloft.local>
2017-01-03 17:40:11 +0800

    初始化Github

---
+++ GitHubTest/BaseVC/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  GitHubTest
+//
+//  Created by caochong on 17/1/3.
+//  Copyright © 2017年 ZLJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ GitHubTest/BaseVC/BaseViewController.m
@@ -0,0 +1,25 @@
+//
+//  BaseViewController.m
+//  GitHubTest
+//
+//  Created by caochong on 17/1/3.
+//  Copyright © 2017年 ZLJ. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+@end
